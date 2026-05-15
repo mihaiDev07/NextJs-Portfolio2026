d19ff93b69c28aa463624ddc5790471fc35332ca
taica07 <taicarullz84@gmail.com>
2026-05-15 13:31:08 +0300

    add animation on Hero Section

--- src/components/HeroOrbit.tsx
+++ src/components/HeroOrbit.tsx
@@ -1,27 +1,47 @@
-import { PropsWithChildren } from "react";
+import { CSSProperties, PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 const HeroOrbit = ({
   children,
   size,
   rotation,
-}: PropsWithChildren<{ size: number; rotation: number }>) => {
+  shouldOrbit = false,
+  orbitDuration,
+  shouldSpin = false,
+  spinDuration,
+}: PropsWithChildren<{
+  size: number;
+  rotation: number;
+  shouldOrbit?: boolean;
+  shouldSpin?: boolean;
+  spinDuration?: string;
+  orbitDuration?: string;
+}>) => {
   return (
-    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 ">
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 -z-10 pointer-events-none">
       <div
-        className="flex items-start justify-start"
+        className={twMerge(shouldOrbit ? "animate-spin" : "")}
         style={{
-          transform: `rotate(${rotation}deg)`,
-          height: `${size}px`,
-          width: `${size}px`,
+          animationDuration: orbitDuration,
         }}
       >
         <div
-          className=" inline-flex"
+          className="flex items-start justify-start"
           style={{
-            transform: `rotate(${rotation * -1}deg)`,
+            transform: `rotate(${rotation}deg)`,
+            width: `min(${size}px, 80vw)`,
+            height: `min(${size}px, 80vw)`,
           }}
         >
-          {children}
+          <div
+            className={twMerge("inline-flex", shouldSpin ? "animate-spin" : "")}
+            style={{
+              animationDuration: spinDuration,
+              transform: `rotate(${rotation * -1}deg)`,
+            }}
+          >
+            {children}
+          </div>
         </div>
       </div>
     </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import memojimage from "@/assets/images/memoji-computer.png";
 import ArrowDown from "@/assets/icons/arrow-down.svg";
@@ -10,7 +12,7 @@ export const HeroSection = () => {
   return (
     <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
       <div
-        className="absolute inset-0
+        className="absolute inset-0 pointer-events-none
        [mask-image:linear-gradient(to-bottom, transparent, black_10%,black_70%,transparent)]
         [-webkit-mask-image:linear-gradient(to_bottom,transparent,black_10%,black_70%,transparent)]
        "
@@ -24,40 +26,89 @@ export const HeroSection = () => {
         <div className="size-[1020px] hero-ring "></div>
         <div className="size-[1220px] hero-ring "></div>
         {/* Animation stars */}
-        <HeroOrbit size={800} rotation={-45}>
-          <StarIcon className="size-28 text-emerald-300 " />
+        <HeroOrbit
+          size={430}
+          rotation={-14}
+          shouldOrbit
+          orbitDuration="30s"
+          shouldSpin
+          spinDuration="4s"
+        >
+          <SparkleIcon className="size-8 text-emerald-300/20 " />
         </HeroOrbit>
-        <HeroOrbit size={550} rotation={20}>
+        <HeroOrbit
+          size={440}
+          rotation={79}
+          shouldOrbit
+          orbitDuration="32s"
+          shouldSpin
+          spinDuration="4s"
+        >
+          <SparkleIcon className="size-5 text-emerald-300/20 " />
+        </HeroOrbit>
+        <HeroOrbit size={520} rotation={-41} shouldOrbit orbitDuration="34s">
+          <div className="size-2 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+        <HeroOrbit
+          size={530}
+          rotation={178}
+          shouldOrbit
+          orbitDuration="36s"
+          shouldSpin
+          spinDuration="4s"
+        >
+          <SparkleIcon className="size-10 text-emerald-300/20 " />
+        </HeroOrbit>
+        <HeroOrbit
+          size={550}
+          rotation={20}
+          shouldOrbit
+          orbitDuration="38s"
+          shouldSpin
+          spinDuration="6s"
+        >
           <StarIcon className="size-12 text-emerald-300 " />
         </HeroOrbit>
-        <HeroOrbit size={590} rotation={98}>
+        <HeroOrbit
+          size={590}
+          rotation={98}
+          shouldOrbit
+          orbitDuration="40s"
+          shouldSpin
+          spinDuration="6s"
+        >
           <StarIcon className="size-8 text-emerald-300 " />
+        </HeroOrbit>
+        <HeroOrbit size={650} rotation={-5} shouldOrbit orbitDuration="42s">
+          <div className="size-2 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+        <HeroOrbit
+          size={710}
+          rotation={144}
+          shouldOrbit
+          orbitDuration="44s"
+          shouldSpin
+          spinDuration="4s"
+        >
+          <SparkleIcon className="size-14 text-emerald-300/20 " />
+        </HeroOrbit>
+        <HeroOrbit size={720} rotation={85} shouldOrbit orbitDuration="46s">
+          <div className="size-3 bg-emerald-300/20 rounded-full" />
+        </HeroOrbit>
+        <HeroOrbit
+          size={800}
+          rotation={-72}
+          shouldOrbit
+          orbitDuration="48s"
+          shouldSpin
+          spinDuration="6s"
+        >
+          <StarIcon className="size-28 text-emerald-300 " />
         </HeroOrbit>
 
         {/* Animation sparkle */}
-        <HeroOrbit size={430} rotation={-14}>
-          <SparkleIcon className="size-8 text-emerald-300/20 " />
-        </HeroOrbit>
-        <HeroOrbit size={440} rotation={79}>
-          <SparkleIcon className="size-5 text-emerald-300/20 " />
-        </HeroOrbit>
-        <HeroOrbit size={530} rotation={178}>
-          <SparkleIcon className="size-10 text-emerald-300/20 " />
-        </HeroOrbit>
-        <HeroOrbit size={710} rotation={144}>
-          <SparkleIcon className="size-14 text-emerald-300/20 " />
-        </HeroOrbit>
 
         {/* Animation circle */}
-        <HeroOrbit size={720} rotation={85}>
-          <div className="size-3 bg-emerald-300/20 rounded-full" />
-        </HeroOrbit>
-        <HeroOrbit size={520} rotation={-41}>
-          <div className="size-2 bg-emerald-300/20 rounded-full" />
-        </HeroOrbit>
-        <HeroOrbit size={650} rotation={-5}>
-          <div className="size-2 bg-emerald-300/20 rounded-full" />
-        </HeroOrbit>
       </div>
 
       <div className="container">
@@ -65,7 +116,12 @@ export const HeroSection = () => {
           <Image src={memojimage} alt="Memoj" className="size-[100px]" />
 
           <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-large">
-            <div className="bg-green-500 size-2.5 rounded-full"></div>
+            <div className="bg-green-500 size-2.5 rounded-full relative ">
+              <div
+                className="absolute inset-0 bg-green-500 
+              rounded-full animate-ping-large"
+              ></div>
+            </div>
             <div className="text-sm font-medium">
               Available for new projects
             </div>
@@ -82,11 +138,19 @@ export const HeroSection = () => {
             </p>
           </div>
           <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
-            <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+            <button
+              onClick={() => console.log("Clicked  1")}
+              className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl cursor-pointer"
+            >
               <span className="font-semibold">Explore my work</span>
               <ArrowDown className="size-4" />
             </button>
-            <button className="inline-flex items-center gap-2 border-x-white bg-white text-gray-900 h-12 px-6 rounded-xl ">
+            <button
+              onClick={() => console.log("Clicked 2")}
+              className="inline-flex items-center gap-2
+             border-x-white bg-white text-gray-900 h-12 
+             px-6 rounded-xl cursor-pointer "
+            >
               <span>👋</span>
               <span>Let&apos;s connect</span>
             </button>
